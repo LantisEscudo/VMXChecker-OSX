b18969e0233db7a921bdca804268c170c3387427
Lantis <user@Karla.local>
2013-01-17 22:32:38 -0500

    Initial Commit

---
+++ VMXChecker/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  VMXChecker
+//
+//  Created by User on 1/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+	
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
